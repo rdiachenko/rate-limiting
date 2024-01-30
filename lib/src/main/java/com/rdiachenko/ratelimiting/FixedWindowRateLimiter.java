a5a9df121e61abce8d5e92b04c78c86a9d71b96f
Ruslan Diachenko <rd.ryly@gmail.com>
2024-01-30 21:24:13 +0000

    format code

--- lib/src/main/java/com/rdiachenko/ratelimiting/FixedWindowRateLimiter.java
+++ lib/src/main/java/com/rdiachenko/ratelimiting/FixedWindowRateLimiter.java
@@ -6,37 +6,41 @@ import java.util.Map;
 
 public class FixedWindowRateLimiter {
 
-    private final int maxCount;
-    private final long windowLengthMillis;
-    private final Clock clock;
-    private final Map<String, FixedWindow> userFixedWindow = new HashMap<>();
+  private final int maxCount;
+  private final long windowLengthMillis;
+  private final Clock clock;
+  private final Map<String, FixedWindow> userFixedWindow = new HashMap<>();
 
-    FixedWindowRateLimiter(int maxCount, long windowLengthMillis, Clock clock) {
-        this.maxCount = maxCount;
-        this.windowLengthMillis = windowLengthMillis;
-        this.clock = clock;
+  FixedWindowRateLimiter(int maxCount, long windowLengthMillis, Clock clock) {
+    this.maxCount = maxCount;
+    this.windowLengthMillis = windowLengthMillis;
+    this.clock = clock;
+  }
+
+  boolean allowed(String userId) {
+    long now = clock.millis();
+    FixedWindow fixedWindow = userFixedWindow.get(userId);
+
+    // If there is a new user OR it is time to start a new window,
+    // initialize a new fixed window with the current request timestamp.
+    if (fixedWindow == null
+        || fixedWindow.timestamp() + windowLengthMillis < now) {
+      fixedWindow = new FixedWindow(now, 0);
     }
 
-    boolean allowed(String userId) {
-        long now = clock.millis();
-        FixedWindow fixedWindow = userFixedWindow.get(userId);
-
-        // If there is a new user OR it is time to start a new window,
-        // initialize a new fixed window with the current request timestamp.
-        if (fixedWindow == null || fixedWindow.timestamp() + windowLengthMillis < now) {
-            fixedWindow = new FixedWindow(now, 0);
-        }
-
-        // If a number of requests within the window exceeds the limit, disallow this request.
-        // Otherwise, update the current request count and allow the request.
-        if (fixedWindow.count() >= maxCount) {
-            return false;
-        } else {
-            userFixedWindow.put(userId, new FixedWindow(fixedWindow.timestamp(), fixedWindow.count() + 1));
-            return true;
-        }
+    // If a number of requests within the window exceeds the limit,
+    // disallow this request. Otherwise, update the current request count
+    // and allow the request.
+    if (fixedWindow.count() >= maxCount) {
+      return false;
+    } else {
+      FixedWindow updatedWindow = new FixedWindow(fixedWindow.timestamp(),
+          fixedWindow.count() + 1);
+      userFixedWindow.put(userId, updatedWindow);
+      return true;
     }
+  }
 
-    private record FixedWindow(long timestamp, int count) {
-    }
+  private record FixedWindow(long timestamp, int count) {
+  }
 }
--- lib/src/test/java/com/rdiachenko/ratelimiting/FixedWindowRateLimiterTest.java
+++ lib/src/test/java/com/rdiachenko/ratelimiting/FixedWindowRateLimiterTest.java
@@ -11,30 +11,41 @@ import static org.mockito.Mockito.when;
 
 public class FixedWindowRateLimiterTest {
 
-    @Test
-    void testLimiter() {
-        Clock clock = mock(Clock.class);
-        when(clock.millis()).thenReturn(0L, 0L, 1000L, 1000L, 1000L, 2001L, 2001L, 2001L, 3001L, 3001L);
+  @Test
+  void testLimiter() {
+    Clock clock = mock(Clock.class);
+    when(clock.millis()).thenReturn(0L, 0L, 1000L, 1000L, 1000L,
+        2001L, 2001L, 2001L, 3001L, 3001L);
 
-        FixedWindowRateLimiter limiter = new FixedWindowRateLimiter(1, 2000, clock);
-        String bob = "Bob";
-        String alice = "Alice";
+    FixedWindowRateLimiter limiter = new FixedWindowRateLimiter(1, 2000, clock);
+    String bob = "Bob";
+    String alice = "Alice";
 
-        assertTrue(limiter.allowed(bob), "First Bob's request must pass");
-        assertFalse(limiter.allowed(bob), "Second Bob's request must not be allowed");
+    assertTrue(limiter.allowed(bob),
+        "First Bob's request must pass");
+    assertFalse(limiter.allowed(bob),
+        "Second Bob's request must not be allowed");
 
-        // 1 second passed
-        assertFalse(limiter.allowed(bob), "Bob's fixed window is not reset yet, request must not be allowed");
-        assertTrue(limiter.allowed(alice), "First Alice's request must pass");
-        assertFalse(limiter.allowed(alice), "Second Alice's request must not be allowed");
+    // 1 second passed
+    assertFalse(limiter.allowed(bob),
+        "Bob's fixed window is not reset yet, request must not be allowed");
+    assertTrue(limiter.allowed(alice),
+        "First Alice's request must pass");
+    assertFalse(limiter.allowed(alice),
+        "Second Alice's request must not be allowed");
 
-        // 2 seconds passed
-        assertFalse(limiter.allowed(alice), "Alice's fixed window is not reset yet, request must not be allowed");
-        assertTrue(limiter.allowed(bob), "Bob's fixed window is reset, request must pass");
-        assertFalse(limiter.allowed(bob), "Second Bob's request must not be allowed");
+    // 2 seconds passed
+    assertFalse(limiter.allowed(alice),
+        "Alice's fixed window is not reset yet, request must not be allowed");
+    assertTrue(limiter.allowed(bob),
+        "Bob's fixed window is reset, request must pass");
+    assertFalse(limiter.allowed(bob),
+        "Second Bob's request must not be allowed");
 
-        // 3 seconds passed
-        assertTrue(limiter.allowed(alice), "Alice's fixed window is reset, request must pass");
-        assertFalse(limiter.allowed(alice), "Second Alice's request must not be allowed");
-    }
+    // 3 seconds passed
+    assertTrue(limiter.allowed(alice),
+        "Alice's fixed window is reset, request must pass");
+    assertFalse(limiter.allowed(alice),
+        "Second Alice's request must not be allowed");
+  }
 }
