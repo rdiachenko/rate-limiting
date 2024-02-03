ff81105bdf8a73350c283b4298af7071a304d5ce
Ruslan Diachenko <rd.ryly@gmail.com>
2024-02-03 21:46:05 +0000

    implement sliding window log algorithm

---
+++ lib/src/main/java/com/rdiachenko/ratelimiting/SlidingWindowLogRateLimiter.java
@@ -0,0 +1,47 @@
+package com.rdiachenko.ratelimiting;
+
+import java.time.Clock;
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class SlidingWindowLogRateLimiter {
+
+  private final int maxCount;
+  private final long windowLengthMillis;
+  private final Clock clock;
+  private final Map<String, Deque<Long>> userSlidingWindow = new HashMap<>();
+
+  SlidingWindowLogRateLimiter(int maxCount, long windowLengthMillis, Clock clock) {
+    this.maxCount = maxCount;
+    this.windowLengthMillis = windowLengthMillis;
+    this.clock = clock;
+  }
+
+  boolean allowed(String userId) {
+    long now = clock.millis();
+
+    // If there is a new user, initialize an empty sliding window for them.
+    // Otherwise, get an existing window which contains timestamps of the
+    // previously made requests.
+    Deque<Long> slidingWindow = userSlidingWindow
+        .computeIfAbsent(userId, k -> new LinkedList<>());
+
+    // Remove request timestamps which are outside the current sliding window.
+    while (!slidingWindow.isEmpty()
+        && slidingWindow.getFirst() + windowLengthMillis < now) {
+      slidingWindow.removeFirst();
+    }
+
+    // If the current number of requests within the window exceeds the limit,
+    // disallow this request. Otherwise, include the current request into
+    // the window and allow the request.
+    if (slidingWindow.size() >= maxCount) {
+      return false;
+    } else {
+      slidingWindow.addLast(now);
+      return true;
+    }
+  }
+}
---
+++ lib/src/test/java/com/rdiachenko/ratelimiting/SlidingWindowLogRateLimiterTest.java
@@ -0,0 +1,88 @@
+package com.rdiachenko.ratelimiting;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Clock;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class SlidingWindowLogRateLimiterTest {
+
+  private static final String BOB = "Bob";
+  private static final String ALICE = "Alice";
+
+  @Test
+  void testLimiterDisallowsBurstyTraffic() {
+    Clock clock = mock(Clock.class);
+    when(clock.millis()).thenReturn(0L, 999L, 1000L,
+        1001L, 1002L, 1999L, 2000L);
+
+    SlidingWindowLogRateLimiter limiter
+        = new SlidingWindowLogRateLimiter(2, 1000, clock);
+
+    // 0 seconds passed
+    assertTrue(limiter.allowed(BOB),
+        "Bob's request 1 at timestamp=0 must pass");
+    assertTrue(limiter.allowed(BOB),
+        "Bob's request 2 at timestamp=999 must pass");
+
+    // 1 second passed
+    assertFalse(limiter.allowed(BOB),
+        "Bob's request 3 at timestamp=1000 must not be allowed");
+    assertTrue(limiter.allowed(BOB),
+        "Bob's request 4 at timestamp=1001 must pass, because request 1" +
+            " at timestamp=0 is outside the current sliding window [1; 1001]");
+    assertFalse(limiter.allowed(BOB),
+        "Bob's request 5 at timestamp=1002 must not be allowed");
+    assertFalse(limiter.allowed(BOB),
+        "Bob's request 6 at timestamp=1999 must not be allowed");
+
+    // 2 seconds passed
+    assertTrue(limiter.allowed(BOB),
+        "Bob's request 7 at timestamp=2000 must pass, because request 2" +
+            " at timestamp=999 is outside the current sliding window [1000; 2000]");
+  }
+
+  @Test
+  void testLimiterForMultipleUsers() {
+    Clock clock = mock(Clock.class);
+    when(clock.millis()).thenReturn(0L, 999L, 1000L, 1000L, 1001L,
+        2001L, 2001L, 2001L, 3002L, 3003L);
+
+    SlidingWindowLogRateLimiter limiter
+        = new SlidingWindowLogRateLimiter(1, 2000, clock);
+
+    // 0 seconds passed
+    assertTrue(limiter.allowed(BOB),
+        "Bob's request 1 at timestamp=0 must pass");
+    assertFalse(limiter.allowed(BOB),
+        "Bob's request 2 at timestamp=999 must not be allowed");
+
+    // 1 second passed
+    assertFalse(limiter.allowed(BOB),
+        "Bob's request 3 at timestamp=1000 must not be allowed");
+    assertTrue(limiter.allowed(ALICE),
+        "Alice's request 1 at timestamp=1000 must pass");
+    assertFalse(limiter.allowed(ALICE),
+        "Alice's request 2 at timestamp=1001 must pass");
+
+    // 2 seconds passed
+    assertFalse(limiter.allowed(ALICE),
+        "Alice's request 3 at timestamp=2001 must not be allowed");
+    assertTrue(limiter.allowed(BOB),
+        "Bob's request 4 at timestamp=2001 must pass, because request 1" +
+            " at timestamp=0 is outside the current sliding window [1; 2001]");
+    assertFalse(limiter.allowed(BOB),
+        "Bob's request 5 at timestamp=2001 must not be allowed");
+
+    // 3 seconds passed
+    assertTrue(limiter.allowed(ALICE),
+        "Alice's request 4 at timestamp=3002 must pass, because request 1" +
+            " at timestamp=1000 is outside the current sliding window [1002; 3002]");
+    assertFalse(limiter.allowed(ALICE),
+        "Alice's request 5 at timestamp=3003 must not be allowed");
+  }
+}
