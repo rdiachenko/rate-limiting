0f8bb0179c7d559a92b8db234a91f7eb55d70efb
Ruslan Diachenko <rd.ryly@gmail.com>
2024-03-23 14:56:48 +0000

    add javadoc for leacky bucket algorithm

--- lib/src/main/java/com/rdiachenko/ratelimiting/LeakyBucketRateLimiter.java
+++ lib/src/main/java/com/rdiachenko/ratelimiting/LeakyBucketRateLimiter.java
@@ -13,6 +13,14 @@ public class LeakyBucketRateLimiter {
   private final Clock clock;
   private final Map<String, LeakyBucket> userLeakyBucket = new HashMap<>();
 
+  /**
+   * Constructs a leaky bucket rate limiter.
+   *
+   * @param capacity       The maximum number of requests a user can make in a given period before being limited.
+   * @param period         The time frame in which requests are considered for limiting.
+   * @param leaksPerPeriod The number of requests that are allowed to leak out (processed) per period.
+   * @param clock          The clock used to determine the current time, facilitating testing with fixed clocks.
+   */
   public LeakyBucketRateLimiter(int capacity, Duration period, int leaksPerPeriod, Clock clock) {
     this.capacity = capacity;
     this.period = period;
@@ -20,6 +28,12 @@ public class LeakyBucketRateLimiter {
     this.clock = clock;
   }
 
+  /**
+   * Determines if a request by a given user ID is allowed under the current rate limiting rules.
+   *
+   * @param userId The user ID making the request.
+   * @return true if the request is allowed, false otherwise.
+   */
   public boolean allowed(String userId) {
     LeakyBucket bucket = userLeakyBucket.computeIfAbsent(userId,
         k -> new LeakyBucket(clock.millis(), 0));
@@ -33,11 +47,21 @@ public class LeakyBucketRateLimiter {
     private long leakTimestamp; // Timestamp of the last leak.
     private long waterLevel; // Current water level represents the number of pending requests.
 
+    /**
+     * Constructs a leaky bucket instance.
+     *
+     * @param leakTimestamp The initial timestamp of the last leak.
+     * @param waterLevel    The initial water level of the bucket.
+     */
     LeakyBucket(long leakTimestamp, long waterLevel) {
       this.leakTimestamp = leakTimestamp;
       this.waterLevel = waterLevel;
     }
 
+    /**
+     * Simulates the leaking of requests over time. This method adjusts the water level
+     * based on the elapsed time since the last leak, applying the defined leak rate.
+     */
     void leak() {
       long now = clock.millis();
       long elapsedTime = now - leakTimestamp;
@@ -50,6 +74,11 @@ public class LeakyBucketRateLimiter {
       }
     }
 
+    /**
+     * Attempts to process a request by incrementing the water level if under capacity.
+     *
+     * @return true if the request is processed (under capacity), false if the bucket is full.
+     */
     boolean processed() {
       if (waterLevel < capacity) {
         ++waterLevel;
